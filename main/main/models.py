ddce4ab4779192bf4b18f085849677f5bb4e8f97
Madalambana <almadalambana@gmail.com>
2022-03-06 15:16:12 +0200

    Second commit

--- main/main/models.py
+++ main/main/models.py
@@ -5,25 +5,16 @@ from sqlalchemy.sql.expression import text
 from sqlalchemy.orm import relationship
 
 
-
-
 class Posts(Base):
     __tablename__ = "posts"
-    
     username = Column(String,ForeignKey('users.username', ondelete="CASCADE"), nullable=False)
     pid = Column(Integer, primary_key=True, nullable=False)
     post = Column(String,nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('Now()'))
-    comments = Column(String, )
+    comments = Column(String)
 
 class Users(Base):
     __tablename__ = "users"
     username = Column(String, nullable=False, primary_key=True)
     studentnr = Column(Integer, nullable=False, unique=True)
     password = Column(String, nullable=False,)
-
-class Comment(Base):
-    __tablename__ = "comments"
-    pid_id = Column(Integer, ForeignKey('posts.pid', ondelete='CASCADE'),primary_key=True, nullable=False)
-    comment = Column(String,ForeignKey('posts.comments', ondelete='CASCADE'), nullable=False )
-
